0653fd78a1dd81b66f6db9a1bc26b0a88ee210d8
Aragogis <88444433+Aragogis@users.noreply.github.com>
2021-11-02 16:08:59 +0200

    conntcting to mongodb, adding user info into db
    also added some comments (todo list)

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
+// нужно дописать парсинг цены с сайта(1), проверку актуальности цены(2), отправление сообщения пользователю, при изменении цены(3) и возможность удаления ссылок из отслеживаемых(4).
+
 const { Telegraf,Scenes, session } = require('telegraf')
 const { leave, enter } = Scenes.Stage
+const { MongoClient } = require('mongodb')
 
 const token = '2094134745:AAENFLbt4bXWCfngzmj-EvMMJg8VdR0nPnc'
 const bot = new Telegraf(token)
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@cluster0.lnmty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    client.close();
-});
+
+const uri = "mongodb+srv://admin:admin@cluster0.lnmty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const collection = client.db("TGBot").collection("Users")
 
 const addFavourite = new Scenes.BaseScene('addFavourite')
 addFavourite.enter((ctx) => ctx.reply('Отправьте ссылку на товар, который вы хотите отслеживать'))
 addFavourite.on('message', async (msg) => {
 
     if (msg.message.text.startsWith('https://rozetka.com.ua/')) {
-        let link = msg.message.text;
+        const link = msg.message.text;
+        const userId = msg.message.chat.id;
+        // const price = тут должна парсится цена с ссылки link  (1)
         await msg.reply('Отлично! Товар добавлен')
+
+        await client.connect()
+        const count = await collection.find({userId : userId}).toArray()
+        if( count.length === 0) await collection.insertOne({"userId" : userId, "links" : [link], "prices" : [price] })
+        else await collection.updateOne({userId : userId}, {$addToSet: {links: link}, $addToSet : {prices : price}})
+        await client.close()
+
     } else {
         await msg.reply('Вы ввели некорректную ссылку')
     }
     await msg.scene.leave();
 })
 
-const stage = new Scenes.Stage([addFavourite]);
-bot.use(session());
-bot.use(stage.middleware());
+const stage = new Scenes.Stage([addFavourite])
+bot.use(session())
+bot.use(stage.middleware())
 
 
 bot.command('info', async (ctx) => {
